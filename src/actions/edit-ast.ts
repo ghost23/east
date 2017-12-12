0d217e9cedb7689c6f4e3cae520ea020c2ccf49c
Sven Busse <mail@ghost23.de>
2017-12-12 16:49:17 +0100

    formatting

--- src/actions/edit-ast.ts
+++ src/actions/edit-ast.ts
@@ -1,6 +1,6 @@
-
 import { Action } from 'redux';
 import * as ESTree from 'estree';
+
 export const UPDATE_AST_NODE_PROPERTY = "UPDATE_AST_NODE_PROPERTY";
 export const SET_AST_SUBTREE = "SET_AST_SUBTREE";
 export const ADD_AST_SUBTREE = "ADD_AST_SUBTREE";
@@ -21,27 +21,25 @@ export interface UpdateASTNodeProperty extends Action, ASTLocation {
 export interface SetASTSubtree extends Action {
 	type: "SET_AST_SUBTREE";
 	insertionPoint: ASTLocation;
-	subTree: ESTree.Node
+	subTree: ESTree.Node;
 }
 
 export interface AddASTSubtree extends Action {
-    type: "ADD_AST_SUBTREE";
-    insertionPoint: ASTLocation;
-    subTree: ESTree.Node
+	type: "ADD_AST_SUBTREE";
+	insertionPoint: ASTLocation;
+	subTree: ESTree.Node;
 }
 
 export interface RemoveASTSubtree extends Action {
-    type: "REMOVE_AST_SUBTREE";
-    insertionPoint: ASTLocation;
+	type: "REMOVE_AST_SUBTREE";
+	insertionPoint: ASTLocation;
 }
 
-export function updateASTNodeProperty(
-		newValue: any,
-		type: string,
-		uid: string,
-		propName: keyof ESTree.Node,
-		propIndex?: number
-): UpdateASTNodeProperty {
+export function updateASTNodeProperty(newValue: string | number | boolean,
+	type: string,
+	uid: string,
+	propName: keyof ESTree.Node,
+	propIndex?: number): UpdateASTNodeProperty {
 	return {
 		type: UPDATE_AST_NODE_PROPERTY,
 		nodeType: type,
@@ -59,13 +57,13 @@ export function setASTSubtree(insertionPoint: ASTLocation, subTree: ESTree.Node)
 }
 
 export function addASTSubtree(insertionPoint: ASTLocation, subTree: ESTree.Node): AddASTSubtree {
-    return {
-        type: ADD_AST_SUBTREE, insertionPoint, subTree
-    };
+	return {
+		type: ADD_AST_SUBTREE, insertionPoint, subTree
+	};
 }
 
 export function removeASTSubtree(insertionPoint: ASTLocation): RemoveASTSubtree {
-    return {
-        type: REMOVE_AST_SUBTREE, insertionPoint
-    };
+	return {
+		type: REMOVE_AST_SUBTREE, insertionPoint
+	};
 }
